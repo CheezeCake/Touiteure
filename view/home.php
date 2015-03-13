a492ca98076ee1aef27174ecf3cafe313fafac2d
CheezeCake <emmanuel.nicolet@gmail.com>
2015-03-13 17:07:21 +0000

    tweets view

--- view/home.php
+++ view/home.php
@@ -14,24 +14,7 @@
 
 			<div class="well">
 
-			<?php foreach ($context->tweets as $tweet): ?>
-				<div class="row">
-					<div class="col-md-12">
-						<?php if (array_key_exists('retweeted_status', $tweet)): ?>
-							<strong>Retweeté</strong>
-						<?php endif; ?>
-						<?php if ($tweet['favorited'] == 'true'): ?>
-							<strong>, mis en favori</strong>
-						<?php endif; ?>
-						<?= $tweet['user']['name'] ?> (<a href="Touiteure.php?action=profile&screen_name=<?= $tweet['user']['screen_name'] ?>">@<?= $tweet['user']['screen_name'] ?></a>)
-						<span class="pull-right"><?= date('D, d M Y H:i:s', strtotime($tweet['created_at'])) ?></span>
-						<p><?= $tweet['text'] ?></p>
-					</div>
-				</div>
-
-				<hr>
-
-			<?php endforeach; ?>
+				<?php include('view/tweets.php'); ?>
 
 			</div>
 
